2bf61e3f5006f89bff70b5b670488f95f179feb8
Simon <simon@localhost>
2023-04-19 21:17:11 +0200

    Add test to I2S module

--- src/test/scala/io/WM8731ControllerSpec.scala
+++ src/test/scala/io/WM8731ControllerSpec.scala
@@ -12,6 +12,56 @@ class WM8731ControllerSpec extends AnyFlatSpec with ChiselScalatestTester {
         dut.clock.step(10)
         dut.reset.poke(false.B)
         dut.clock.step(150000)
+
+        // now after finishing the i2c configuration test the input
+        dut.io.outData(0).poke(0x123456.S)
+        dut.io.outData(1).poke(0x0bcdef.S)
+        dut.clock.step(1) // sync outData inner register
+
+        val adcSignal = Array(
+            // channel 0
+            0, // ignored
+            0, 0, 0, 1, // 1
+            0, 1, 1, 0, // 6
+            0, 0, 1, 1, // 3
+            0, 0, 1, 1, // 3
+            1, 1, 1, 1, // f
+            1, 1, 1, 1, // f
+            0, 0, 0, 0, 0, // pad to 30
+
+            // channel 1
+            0, // ignored
+            1, 0, 0, 1, // 9
+            1, 1, 1, 0, // e
+            1, 0, 1, 1, // b
+            1, 0, 1, 1, // b
+            0, 1, 1, 1, // 7
+            0, 1, 1, 1, // 7
+            0, 0, 0, 0, 0, // pad to 30
+        )
+
+        // do twice
+        for (j <- 0 until 2) {
+            for (i <- 0 until 30 * 2 * 2) {
+              // first 0 channel, then 1 channel, give 30 clocks in each channel
+              if ((i / (30 * 2)) == 0) {
+                dut.io.wm8731io.dac.daclrck.poke(true.B)
+                dut.io.wm8731io.adc.adclrck.poke(true.B)
+              } else {
+                dut.io.wm8731io.dac.daclrck.poke(false.B)
+                dut.io.wm8731io.adc.adclrck.poke(false.B)
+              }
+
+              // clock bclk
+              if (i % 2 == 0) {
+                dut.io.wm8731io.bclk.poke(false.B)
+                dut.io.wm8731io.adc.adcdat.poke(adcSignal(i / 2).B)
+              } else {
+                dut.io.wm8731io.bclk.poke(true.B)
+              }
+              dut.clock.step(40)
+            }
+        }
     }
   }
 }
