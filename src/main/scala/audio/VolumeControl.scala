0eef7e9b2755a63d868fdf9f0d073a1f4eb1c6e4
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-05-10 12:37:21 +0200

    fixed bug in volume ctrler

--- src/main/scala/audio/VolumeControl.scala
+++ src/main/scala/audio/VolumeControl.scala
@@ -5,7 +5,7 @@ import utility.Constants.{CTRL_WIDTH, DATA_WIDTH}
 
 class VolumeControl(defaultCtrl: Int = 16) extends DSPModule(defaultCtrl) {
   // Create a fixed point (24.4) version of audio signal
-  val audioInFix = audioInReg ## 0.S((CTRL_WIDTH/2).W)
+  val audioInFix = (audioInReg ## 0.S((CTRL_WIDTH/2).W)).asSInt
 
   // Multiply by control signal and shift back to non fixed point
   val postGain = ((audioInFix*ctrlReg.zext) >> CTRL_WIDTH)(DATA_WIDTH - 1, 0).asSInt
