8c17946f72fd5cea4b5a742a64828b02cdd4b314
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-05-11 15:08:40 +0200

    distortion now scales better to real audio input

--- src/main/scala/audio/ClampDistortion.scala
+++ src/main/scala/audio/ClampDistortion.scala
@@ -4,9 +4,11 @@ import chisel3._
 import scala.math.pow
 import utility.Constants.{DATA_WIDTH}
 
-class ClampDistortion(defaultCtrl: Int = 16) extends VolumeControl(defaultCtrl) {
+class ClampDistortion(defaultCtrl: Int = 16, clampFrac: Int = 256) extends VolumeControl(defaultCtrl) {
   // Calculate clamping value
-  val clampUpper = (pow(2.toFloat, (DATA_WIDTH - 1).toFloat).toInt / 4096).S
+  // clampFrac determines how large the signal should be before the modules clamps it
+  // a value of 4 would mean the signal clamps at a quarter it maximum value
+  val clampUpper = (pow(2.toFloat, (DATA_WIDTH - 1).toFloat).toInt / clampFrac).S
   val clampLower = -clampUpper
 
   when (postGain > clampUpper) {
